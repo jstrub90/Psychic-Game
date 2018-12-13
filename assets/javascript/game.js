3ea6512c22c80b11270e925ad3c7ebca384a70b2
J.Strub <jstrub90@gmail.com>
2018-12-13 14:57:06 -0500

    added randomIndex variable is js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,21 @@ var losses = 0;
 var guessesLeft = 9;
 var guessedLetters = []
 
+var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guesses-left-text");
 var guessesText = document.getElementById("guesses-text");
 
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(computerChoice);
+    var randomIndex = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var computerChoice = alphabet[randomIndex];
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
     if(userGuess === computerChoice){
         wins++;
-        winsText.textContent = "Wins: " + wins;  
-        var winsText = document.getElementById("wins-text");  
+        winsText.textContent = "Wins: " + wins;    
     }else{
         guessesLeft--;
         guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
