f8e851fe818782330618f9fd0a034e5a5f27fed3
J.Strub <jstrub90@gmail.com>
2018-12-13 13:49:45 -0500

    added variables for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
