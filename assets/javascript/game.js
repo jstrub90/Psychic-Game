9534b80bc9f247e5f4a487770a10e7e8a8116fd7
J.Strub <jstrub90@gmail.com>
2018-12-13 14:52:47 -0500

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,32 @@ var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessedLetters = []
+
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guesses-left-text");
+var guessesText = document.getElementById("guesses-text");
 
 
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+
+    if(userGuess === computerChoice){
+        wins++;
+        winsText.textContent = "Wins: " + wins;  
+        var winsText = document.getElementById("wins-text");  
+    }else{
+        guessesLeft--;
+        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    }
+    if(guessesLeft = 0){
+        losses++;
+        lossesText.textContent = "Losses: " + losses;
+    }
+
+    guessesText.textContent = "Your guesses so far: " + userGuess;
+
+}
